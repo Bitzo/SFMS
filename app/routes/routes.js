8c3d21684f263702b43f48b8955c56f473f77b05
snail <544207706@qq.com>
2016-11-06 19:40:27 +0800

    Add:用户模块开发

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,8 +1,19 @@
+/** 
+ * @Author: snail
+ * @Date:   2016-11-6 14:33:36
+ * @Last Modified by:   
+ * @Last Modified time: 
+ */
 var index=require('./index');
 
-var wechat=require('./wechat');
+var weChat=require('./wechat');
+
+var userApi=require('../api/userbiz');
 
 module.exports = function (app) {
     app.use('/', index);
-    app.use('/wechat', wechat);
+    app.use('/wechat', weChat);
+
+    //API相关
+    app.use('/api/user',userApi);
 };
