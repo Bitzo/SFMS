1bafee63336e9afb2243a2e8b655685d1b617153
Duncan <1466863073@qq.com>
2016-11-15 20:00:39 +0800

    update:孙茂昀:用户模块的10，11功能点

--- app/config/config.js
+++ app/config/config.js
@@ -17,10 +17,10 @@ var config = {
     port: 3000, // 程序运行的端口
     host: '127.0.0.1',
     mysql: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: '',
-        database: 'jinkebro',
+        host: '139.224.51.160',
+        user: 'jinkebro_common_user',
+        password: 'jinkebrocommon1320',
+        database: 'jit_backend',
         connectionLimit: 100,
         supportBigNumbers: true,
     },
