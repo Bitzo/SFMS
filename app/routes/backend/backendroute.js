4102cd01d08ac0531368a12958d9d23557497c16
bitzo <bitzo@qq.com>
2016-11-16 10:15:13 +0800

    updata: 用appRequire代替require

--- app/routes/backend/backendroute.js
+++ app/routes/backend/backendroute.js
@@ -5,12 +5,12 @@ var addMenu = appRequire('routes/backend/menu/addmenu');
 var queryAllMenus = appRequire('routes/backend/menu/queryallmenus');
 var updateMenu = appRequire('routes/backend/menu/updatemenu');
 
-var roleroute = require('./role/roleroute');
-var rolefuncroute = require('./role/rolefuncroute');
+var roleroute = appRequire('routes/backend/role/roleroute');
+var rolefuncroute = appRequire('routes/backend/role/rolefuncroute');
 
-var userroute=require('./user/userroute')
-var funcroute=require('./function/functionroute');
-var userroleroute=require('./user/userroleroute')
+var userroute=appRequire('routes/backend/user/userroute')
+var funcroute=appRequire('routes/backend/function/functionroute');
+var userroleroute=appRequire('routes/backend/user/userroleroute')
 
 var userService = appRequire('service/backend/user/userservice');
 var jwtHelper=appRequire('util/jwthelper');
@@ -69,27 +69,14 @@ router.post('/login', function(req, res) {
   })
 });
 
-<<<<<<< HEAD
-
-
-
 router.use('/addmenu',addMenu);
 router.use('/queryallmenus',queryAllMenus);
 router.use('/updatemenu',updateMenu);
-
-
-
-// router.get('/addmenu',function(req,res,next) {
-// 	res.json({
-// 		title : 'zengjiacaidanjiemian'
-// 	});
-// });
-module.exports = router;
-=======
 router.use('/role', roleroute);
 router.use('/rolefunc', rolefuncroute);
 router.use('/function',funcroute);
 router.use('/user',userroute);
 router.use('/userrole',userroleroute);
+
 module.exports = router;
->>>>>>> 72e0bbbf919bab737fc4907c3c8f52951da85893
+
