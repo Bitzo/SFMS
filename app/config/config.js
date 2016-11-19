af9b3faf12a0109f846559799b16f3e77829fb0b
孙茂昀 <1466863073@qq.com>
2016-11-19 18:45:34 +0800

    Update:修改config里的测试数据

--- app/config/config.js
+++ app/config/config.js
@@ -19,15 +19,10 @@ var config = {
     host: '127.0.0.1',
     mysql: {
 
-        // host: '139.224.51.160',
-        // user: 'jinkebro_common_user',
-        // password: 'jinkebrocommon1320',
-        // database: 'jit_backend',
-
-        host: '127.0.0.1',
-        user: 'root',
-        password: '',
-        database: 'jinkebro',
+        host: '139.224.51.160',
+        user: 'jinkebro_common_user',
+        password: 'jinkebrocommon1320',
+        database: 'jit_backend',
         connectionLimit: 100,
         supportBigNumbers: true,
 
