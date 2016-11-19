10fcb24585475557bc50a32a87417fb509883c9c
孙茂昀 <1466863073@qq.com>
2016-11-19 18:42:07 +0800

    Update:去除账号重复的用户

--- app/config/config.js
+++ app/config/config.js
@@ -19,12 +19,18 @@ var config = {
     host: '127.0.0.1',
     mysql: {
 
-        host: '139.224.51.160',
-        user: 'jinkebro_common_user',
-        password: 'jinkebrocommon1320',
-        database: 'jit_backend',
+        // host: '139.224.51.160',
+        // user: 'jinkebro_common_user',
+        // password: 'jinkebrocommon1320',
+        // database: 'jit_backend',
+
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'jinkebro',
         connectionLimit: 100,
         supportBigNumbers: true,
+
     },
      pageCount: 20,//分页时每一页要显示的数据量
     // 邮箱配置
