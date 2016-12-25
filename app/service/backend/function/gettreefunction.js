a12d6108efcf04b0c0262d9981ce92e862a337cc
罗志强 <710277267@qq.com>
2016-12-25 23:02:28 +0800

    Update:功能点树形结构更新

--- app/service/backend/function/gettreefunction.js
+++ app/service/backend/function/gettreefunction.js
@@ -2,17 +2,19 @@
  * @Author: luozQ
  * @Date:   2016-11-14 22:42:38
  * @Last Modified by:   luozQ
- * @Last Modified time: 2016-11-15 10:12:38
+ * @Last Modified time: 2016-12-25 10:12:38
  *  @Function: 将功能点转成多层树形结构
  */
 var appService = appRequire('service/backend/application/applicationservice');
 var logger = appRequire("util/loghelper").helper;
 
-function treeNode(appid, id, pId, name, children) {
-    this.appid = appid;
-    this.id = id;
-    this.pId = pId;
-    this.name = name;
+function treeNode(funcModel, children) {
+    this.ApplicationID = funcModel.ApplicationID;
+    this.FunctionID = funcModel.FunctionID;
+    this.ParentID = funcModel.ParentID;
+    this.name = funcModel.name;
+    this.FunctionName=funcModel.FUnctionName;
+    this.Memo=funcModel.Memo;
     this.children = children;
 }
 //递归生成多层树结构
@@ -20,7 +22,7 @@ function getMultiTree(data, pid) {
     var treelist = [];
     for (var i = 0; i < data.length; i++) {
         if (data[i].ParentID == pid) {
-            var tree = new treeNode(data[i].ApplicationID, data[i].FunctionID, data[i].ParentID, data[i].FunctionName, getMultiTree(data, data[i].FunctionID));
+            var tree = new treeNode(data[i], getMultiTree(data, data[i].FunctionID));
             treelist.push(tree)
         }
     }
@@ -41,9 +43,9 @@ function getTreeFunction(data, callback) {
             var list = [];
             //将应用作用应用的根节点
             for (var j = 0; j < results.length; j++) {
-                var tree = { 'id': -1, 'pId': -1, 'name': results[j].ApplicationName, children: [] };
+                var tree = { 'FunctionID': -1, 'ParentID': -1, 'name': results[j].ApplicationName,'FunctionLevel':'0',children: [] };
                 for (var i = 0; i < treelist.length; i++) {
-                    if (treelist[i].appid == results[j].ID) {
+                    if (treelist[i].ApplicationID == results[j].ID) {
                         tree.children.push(treelist[i]);
                     }
                 }
