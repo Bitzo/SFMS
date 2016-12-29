7c71d53d00d3a6f6f116208e20090f1d1848943a
JaosnCiCi <824930117@qq.com>
2016-12-29 10:28:01 +0800

    liandong

--- app/public/js/angularjs/controller/selectController.js
+++ app/public/js/angularjs/controller/selectController.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2016/12/14.
  */
-myApp.controller('sfmsIndexController', function($scope, $http,$q,baseService) {
+myApp.controller('selectController', function($scope, $http,$q,baseService) {
        //应用名称
         $http({
             method:'get',
@@ -75,7 +75,30 @@ myApp.controller('sfmsIndexController', function($scope, $http,$q,baseService) {
         error(function(response) {
             console.log(response)
         });
+    //所在班级
+    $scope.collegeChanged = function() {
+        console.log($scope.formdata.CollegeID)
+        $http({
+            method:'get',
+            url: '/datadict/plain' +"?access_token="+localStorage.getItem('jit_token')+"&jitkey="+localStorage.getItem('jit_key'),
+            params:{
+                pageindex:1,
+                pagesize:10,
+                f:{
+                    Category:"dc_cls",
+                    ParentID:$scope.formdata.CollegeID
+                }
+            }
+        }).
+        success(function(response) {
+            console.log(response)
+            $scope.cls=response.data;
+        }).
+        error(function(response) {
+            console.log(response)
+        });
 
+    }
       
 
 })
