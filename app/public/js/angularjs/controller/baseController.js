65f63df3107a8082e6c4dc84849ebbea99d90238
ljb <lijingbojs@163.com>
2017-01-17 09:33:12 +0800

    用户菜单编辑页面基本完成

--- app/public/js/angularjs/controller/baseController.js
+++ app/public/js/angularjs/controller/baseController.js
@@ -44,7 +44,7 @@ myApp.controller('baseController', function($scope, $http,$q,baseService) {
     
     //首页 查询
     $scope.search=function(){
-         $scope.paginationConf.currentPage = 1;
+        $scope.paginationConf.currentPage = 1;
         getInit();
         $scope.formdata={};
     }
