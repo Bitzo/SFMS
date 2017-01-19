a7959c23db9a3600d0bf9adecf5700e7e80f570d
ljb <lijingbojs@163.com>
2017-01-19 11:39:55 +0800

    页面根据用户显示已经勾选菜单，不知数据接口尚无法提交

--- app/public/js/angularjs/controller/userMenuController.js
+++ app/public/js/angularjs/controller/userMenuController.js
@@ -4,13 +4,51 @@
 
 myApp.controller('userMenuController', function($scope, $http,$q,baseService,$location) {
        $scope.tree_data = [];
-
+       //获取树形菜单数据
        $http.get('/backmenu?'+"access_token=" + localStorage.getItem('jit_token') + "&jitkey=" + localStorage.getItem('jit_key'))
        .success(function (response) {
             $scope.tree_data = response.data.Menu;
             console.log($scope.tree_data);
         });
-       
+        
+        //获取该用户的菜单信息
+        var account = $location.search().AccountID;
+        console.log(account);
+        $http.get('/usermenurole/userID/'+$location.search().AccountID+"?access_token=" + localStorage.getItem('jit_token') + "&jitkey=" + localStorage.getItem('jit_key'))
+       .success(function (response) {
+            console.log(response.data.Menu);
+            $scope.menuTree = response.data.Menu || [];
+            console.log($scope.menuTree);
+            console.log($scope.tree_data);
+            $scope.tree_data.map(function (data, index) {
+                    foreachtree(data);
+                }
+            );
+            console.log($scope.tree_data);
+        });
+        //显示已经勾选的用户菜单
+        function foreachtree(data){
+            if(data.children&&data.children.length!=0){
+                data.children.map(function(branch){
+                    foreachtree(branch);
+                })
+            }
+            var menuTree= $scope.menuTree;
+            for(var i=0;i<menuTree.length;i++)
+            {
+                if(menuTree[i].MenuID==data.MenuID){
+                    data.myselected=true;
+                    break;
+                }
+            }
+            if(i==menuTree.length){
+                data.myselected=false;
+            }else {
+                data.myselected = true;
+            }
+        }
+
+        //勾选点击效果
         $scope.clickHander=function(branch,parent){
         if(branch.myselected==false) {
             parent.myselected = false;
@@ -26,7 +64,7 @@ myApp.controller('userMenuController', function($scope, $http,$q,baseService,$lo
             branch.myselected=val;
             branch.expanded=true;
         }
-
+        //确认提交传递数据
         $scope.submit=function(){
         var  data=[];
         $scope.tree_data.map(function(tree){
@@ -52,6 +90,7 @@ myApp.controller('userMenuController', function($scope, $http,$q,baseService,$lo
         //     })
         // });
         }
+        //获取勾选菜单的ID
         function foreachsubmit(data,dataparam){
             if(data.children&&data.children.length!=0){
                 data.children.map(function(branch){
