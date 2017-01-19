412e1fe2bfdb0edd2adaf69844f2939ca187385e
ljb <lijingbojs@163.com>
2017-01-19 11:19:27 +0800

    ‘获取到菜单全部树形数据，但是尚不显示菜单名称

--- app/public/js/angularjs/controller/userMenuController.js
+++ app/public/js/angularjs/controller/userMenuController.js
@@ -2,13 +2,15 @@
  * Created by Administrator on 2017/1/11.
  */
 
-myApp.controller('userMenuController', function($scope, $http,$q,baseService) {
+myApp.controller('userMenuController', function($scope, $http,$q,baseService,$location) {
        $scope.tree_data = [];
-       $http.get("/userrole/userID/1?access_token=" + accesstokenstring)
-        .success(function (response) {
+
+       $http.get('/backmenu?'+"access_token=" + localStorage.getItem('jit_token') + "&jitkey=" + localStorage.getItem('jit_key'))
+       .success(function (response) {
             $scope.tree_data = response.data.Menu;
             console.log($scope.tree_data);
         });
+        
 
        
 })
