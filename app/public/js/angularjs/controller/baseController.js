88b883819d373e8e43d30503714b6601cf59a187
bitzo <bitzo@qq.com>
2017-02-04 18:57:37 +0800

    update:修改代码

--- app/public/js/angularjs/controller/baseController.js
+++ app/public/js/angularjs/controller/baseController.js
@@ -71,29 +71,6 @@ myApp.controller('baseController', function($scope, $http,$q,baseService,$locati
     $scope.remove = function(index,a,action){
         var mymessage=confirm("是否确认删除  "+a);  
         if(mymessage==true){
-<<<<<<< HEAD
-        $scope.d={
-            "AccountID":$scope.datas[index].AccountID,
-            "MenuID":$scope.datas[index].MenuID,
-            "ID":$scope.datas[index].ID,
-            "RoleID" : $scope.datas[index].RoleID
-        };
-        $http({
-            method:'delete',
-            url:action+"?access_token="+localStorage.getItem('jit_token')+"&jitkey="+localStorage.getItem('jit_key'),
-            params:{
-                d:$scope.d
-            }
-        }).
-        success(function(response) {   
-            alert(response.msg)  
-        }).
-        error(function(response) {
-            alert(response.msg)
-        });
-        $scope.datas.splice(index,1);
-        location.reload();
-=======
             $scope.d={
                 "AccountID":$scope.datas[index].AccountID,
                 "MenuID":$scope.datas[index].MenuID,
@@ -116,11 +93,10 @@ myApp.controller('baseController', function($scope, $http,$q,baseService,$locati
             error(function(response) {
                 alert("操作失败")
             });
->>>>>>> 4044fac70db33aef2b33d79608cf85c216e326d1
         }else{
 
         }
-    }
+    };
 
     //新增页面  添加
     $scope.formdata={};
