527f371cc4fb06a429ec58c7321ff500e5b0dbd0
ljb <lijingbojs@163.com>
2017-03-01 12:15:35 +0800

    update：页面完善

--- app/public/js/angularjs/controller/userRoleController.js
+++ app/public/js/angularjs/controller/userRoleController.js
@@ -25,7 +25,7 @@ myApp.controller('userRoleController', function($scope, $http,$q,baseService,$lo
             $http.get('/userrole/userID/'+$location.search().AccountID+"?access_token=" + localStorage.getItem('jit_token') + "&jitkey=" + localStorage.getItem('jit_key'))
             .success(function (response) {
                     console.log(response)  
-                    $scope.roleTree = response.data || [];  
+                    $scope.roleTree = response.data || [];                  
                     $scope.tree_data.map(function (data, index) {
                             console.log(data+index)
                             foreachtree(data);
