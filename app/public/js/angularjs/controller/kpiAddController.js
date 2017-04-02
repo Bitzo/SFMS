139268ace0c556a4cf7e13e2b83d2149b4d292da
bitzo <bitzo@qq.com>
2017-04-02 20:05:50 +0800

    update：绩效新增，界面修改，可添加图片并预览，与后端的交互暂时未完成。

--- app/public/js/angularjs/controller/kpiAddController.js
+++ app/public/js/angularjs/controller/kpiAddController.js
@@ -7,7 +7,8 @@
  */
 myApp.controller('kpiAddController', function($scope, $http,$q,baseService) {
 
-    //实验室管理系统-绩效管理-新增页面-项目名称  动态变化
+
+        //实验室管理系统-绩效管理-新增页面-项目名称  动态变化
     $scope.userKpiChanged = function() {
         console.log($scope.formdata.UserID)
         $http({
@@ -31,7 +32,6 @@ myApp.controller('kpiAddController', function($scope, $http,$q,baseService) {
     }
 
     //实验室管理系统-绩效管理-编辑页面-项目名称  初始化
-    console.log($scope.paginationConf.formdata.UserID);
     $http({
         method:'get',
         url: '/sfms/api/project/user' +"?access_token="+localStorage.getItem('jit_token')+"&jitkey="+localStorage.getItem('jit_key'),
@@ -95,15 +95,104 @@ myApp.controller('kpiAddController', function($scope, $http,$q,baseService) {
     //bootstrap-fileinput 组件相关
 
     //初始化
-    $("#file").fileinput({
-        uploadUrl: '#', // you must set a valid URL here else you will get an error
-        allowedFileExtensions : ['jpg', 'png','gif'],
-        overwriteInitial: false,
-        maxFileSize: 1000,
-        maxFilesNum: 10,
-        //allowedFileTypes: ['image', 'video', 'flash'],
-        slugCallback: function(filename) {
-            return filename.replace('(', '_').replace(']', '_');
+    // $("#file").fileinput({
+    //     uploadUrl: '#', // you must set a valid URL here else you will get an error
+    //     allowedFileExtensions : ['jpg', 'png','gif'],
+    //     overwriteInitial: false,
+    //     maxFileSize: 1000,
+    //     maxFilesNum: 10,
+    //     //allowedFileTypes: ['image', 'video', 'flash'],
+    //     slugCallback: function(filename) {
+    //         return filename.replace('(', '_').replace(']', '_');
+    //     }
+    // });
+
+    $scope.removeFile = function (item) {
+        var mymessage=confirm("是否确认删除此项");
+        if(mymessage){
+            $scope.files.splice(item.$index,1);
+        }
+    };
+
+    function getObjectURL(file) {
+        var url = null;
+        if (window.createObjectURL!=undefined) { // basic
+            url = window.createObjectURL(file);
+        } else if (window.webkitURL!=undefined) { // webkit or chrome
+            url = window.webkitURL.createObjectURL(file);
+        } else if (window.URL!=undefined) { // mozilla(firefox)
+            url = window.URL.createObjectURL(file);
+        }
+        return url ;
+    }
+
+    function bytesToSize(bytes) {
+        if (bytes === 0) return '0 B';
+        var k = 1000, // or 1024
+            sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+            i = Math.floor(Math.log(bytes) / Math.log(k));
+
+        return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+    }
+
+    $scope.files = [];
+    var file = '';
+    var tempFile = '';
+    var files = [];
+    $("#file").change(function(){
+        tempFile = this.files[0];
+        console.log(tempFile)
+        var objUrl = getObjectURL(this.files[0]);
+        var name = tempFile.name;
+        if (tempFile.name.length > 20) {
+            name = tempFile.name.substring(0, 6) + '......' + tempFile.name.substring(tempFile.name.length-8);
+        }
+        file = {
+            'url': objUrl,
+            'fileName': name,
+            'size': bytesToSize(tempFile.size)
         }
     });
+
+    $scope.addFile = function () {
+        if(file === '') {
+            alert('请选择文件！');
+            return;
+        }
+        $scope.files.push(file);
+        files.push(tempFile);
+        file = '';
+        tempFile = '';
+    };
+
+    $scope.submit = function () {
+        console.log(files);
+
+        if(files.length == 0){
+            alert('请选择文件');
+            return;
+        }else{
+
+            $http({
+                method:'post',
+                headers: {
+                    'Content-Type': undefined
+                },
+                url:"sfms/api/kpi?access_token="+localStorage.getItem('jit_token')+"&jitkey="+localStorage.getItem('jit_key'),
+
+                data:{
+                    file: files
+                }
+            }).
+            success(function(response) {
+                alert(response.msg);
+            }).
+            error(function(response) {
+                alert(response.msg);
+            });
+
+            alert('OK');
+        }
+    }
+
 });
